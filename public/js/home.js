1579d83055549476a2d306be1b65ac762b00fc89
victor.taguines <victor.taguines@republisys.com>
2017-04-21 09:45:30 +0800

    commit transition_div

--- public/js/home.js
+++ public/js/home.js
@@ -2,24 +2,48 @@
  * Created by victor.taguines on 4/19/17.
  */
 $(document).ready(function () {
+    var num = 0;
     $('.transition_btn').click(function () {
-        if ($('.transition_div').css('width') == '100px') {
+        if (num == 0) {
+            num = 1;
             $('.transition_div').css({
                 width: '100%',
                 height: '100%',
                 borderBottomLeftRadius: '0'
             });
             $('.dp_container').css({
-                opacity: 1
+                width: '100%',
+                height: '100%',
+                opacity: 1,
+                visibility:'visible',
+                borderBottomLeftRadius: '0',
+                zIndex:'1'
+            });
+            $('.profile_container').css({
+                visibility:'visible',
+                borderBottomLeftRadius:'0'
+            });
+            $('.transition_btn').css({
+                zIndex:'2'
             });
         } else {
+            num = 0;
             $('.transition_div').css({
                 width: '100px',
                 height: '100px',
                 borderBottomLeftRadius: '100%'
             });
             $('.dp_container').css({
-                opacity: 0
+                opacity: 0,
+                zIndex:'1',
+                borderBottomLeftRadius: '100%',
+                background:'#0074D9'
+            });
+            $('.transition_btn').css({
+                zIndex:'2'
+            });
+            $('.profile_container').css({
+                borderBottomLeftRadius:'100%'
             });
         }
     });
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -17,8 +17,89 @@
     <div class="col-md-6 yield">@include('yield.mid_side')</div>
     <div class="col-md-3 yield">@include('yield.right_side')</div>
     <div class="transition_div">
-        <div class="transition_btn"></div>
-        <p>test</p>
+        <div class="transition_btn">
+            <img id="img_profile_pic_click" src="/images/profile.jpg" />
+        </div>
+        <div class="dp_container">
+            <div class="profile_container">
+                <img id="img_cover_pic" src="/images/sample.png" />
+                <div class="change_dp_container"><img id="img_profile_pic" src="/images/profile.jpg" /></div>
+            </div>
+            <div class="info_container">
+                <div class="area_container">
+                    <div class="area_left_container">
+                        <form id="form-aboutYou" class="form-aboutYou" method="POST">
+                            <div class="form-group col-lg-12">
+                                <label for="formGroupExampleInput">Full Name</label>
+                                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <label for="formGroupExampleInput2">Nickname</label>
+                                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <label for="formGroupExampleInput2">Address</label>
+                                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <label for="formGroupExampleInput2">Age</label>
+                                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <label for="formGroupExampleInput2">Gender/Sex</label>
+                                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <label for="formGroupExampleInput2">Nationality</label>
+                                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <label for="formGroupExampleInput2">Place of Birth</label>
+                                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <label for="formGroupExampleInput2">Date of Birth</label>
+                                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                            </div>
+                    </div>
+                    <div class="area_right_container">
+                            <div class="form-group col-lg-12">
+                                <label for="formGroupExampleInput">School</label>
+                                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <label for="formGroupExampleInput2">Work</label>
+                                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <label for="formGroupExampleInput2">Professional Skills</label>
+                                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <label for="formGroupExampleInput2">Email</label>
+                                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <label for="formGroupExampleInput2">Phone Number</label>
+                                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <label for="formGroupExampleInput2">Status</label>
+                                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <label for="formGroupExampleInput2">Place of Birth</label>
+                                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <label for="formGroupExampleInput2">Date of Birth</label>
+                                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 </body>
